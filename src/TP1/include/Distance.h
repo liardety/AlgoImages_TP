84a30451a5c74d73c40c80b22782f142b0dd5080
Yoan LIARDET <yoan.liardet@poste.isima.fr>
2016-01-26 18:38:39 +0100

    codage bourrin

--- src/TP1/include/Distance.h
+++ src/TP1/include/Distance.h
@@ -6,6 +6,9 @@
 #define ALGOIMAGE_DISTANCE_H
 
 #include <math.h>
+#include "../include/CImg.h"
+
+using cimg_library::Cimg<T>
 
 template <typename T>
 struct Distance {
@@ -18,6 +21,10 @@ struct Distance {
 
 template <typename T>
 struct EuclidianDistance : public Distance<T> {
+  //  première implémentation "sale"
+  CImg<T> operator()(const T & nu, const CImg<T> origine)const{
+    return origine.get_distance(nu,2);
+  }
 };
 
 #endif //ALGOIMAGE_DISTANCE_H
--- src/TP1/sample/KFCM.cpp
+++ src/TP1/sample/KFCM.cpp
@@ -6,25 +6,80 @@
 #include <iostream>
 #include "../include/CImg.h"
 
+#include "KFCM.h"
+#include "Distance.h"
+#include "Kernel.h"
+
 using namespace cimg_library;
 
 static const int nbCluster = 2;
 static const unsigned int tMax = 10;
 static const double epsilon = 0.01;
+static const double fuzzyCoef = 2;
 
-int main(int argc,char **argv) {
+int main(int argc, char **argv) {
     const std::string fileLocation("");
     const std::string filename("./img/firstSample.bmp");
 
-    CImg<> origin((fileLocation + filename).c_str());
+    const CImg<> origin((fileLocation + filename).c_str());
     CImgList<> clusters(nbCluster, origin.width(), origin.height(), 1,1, 1.0 / float(nbCluster) );
+    CImgList<> clustersSave(clusters); // sauvegarde de l'itération précédente pour le critaire d'arrêt.
+    std::vector<float> nu(nbCluster);
+
+    // initialisation des nu
+    for(unsigned int i = 0; i < nbCluster; ++i){
+        nu.push_back((i / nbCluster) * 255.0);
+    }
+
+
 
     std::cout << epsilon << std::endl;
     CImgDisplay disp(origin,"Input Image");
+    
 
-    boolean end = false;
-    for(unsigned int i = 0 ; !end && i < tMax; ++i) {
+    unsigned int tailleBuffer = origin.width() * origin.height();
+    bool end = false;
+    for(unsigned int t = 0 ; !end && t < tMax; ++t) {  // iteration principale
+        
+        // mise à jour des nu
+        for(unsigned int i = 0; i < nbCluster; ++i) { // iteration sur les clusters
+            float* ptrCluster = clusters[i].data();
+            const float* ptrOrigin = origin.data();
+            float denomi = 0;
+            float nume = 0;
+            for( unsigned int k = 0; k < tailleBuffer; ++k ){  // iteration sur les pixels
 
+                float computedKernel = std::pow(*(ptrCluster + k), fuzzyCoef) * Kernel<float, EuclidianDistance<float>>(*(ptrOrigin + k), nu[i]);
+                denomi += computedKernel;
+                nume += computedKernel * (ptrOrigin + k);
+            }
+            assert(denomi == 0);
+            nu[i] = nume / denomi;
+        }
+        
+
+        // mise à jour des clusters 
+        const float* ptrOrigin = Origin.data();
+        for( unsigned int k = 0; k < tailleBuffer; ++k){  // iteration sur les pixels 
+          float denomi = 0;
+          for(unsigned int j = 0; j < nbCluster; ++j){  // calcul de dénominateur nécessitant une sommation pour toutes les classes
+            denomi += std::pow(1 - Kernel<float, EuclidianDistance<float>>(*(ptrOrigin + k), nu[j]));
+          }
+          assert(denomi == 0);
+          for(unsigned int i = 0; i < nbCluster; ++i){  // calcul pour chaque classe
+            *(clusters[i].data() + k) = std::pow(1 - Kernel<float, EuclidianDistance<float>>(*(ptrOrigin + k), nu[i])) / denomi;
+          }
+        }
+        
+        //test d'arrêt
+
+        float maxi = 0;
+        for(unsigned int i = 0; i < nbCluster; ++i){  // iteration sur les classes
+            for(unsigned int k = 0; k < tailleBuffer; ++k){  // iteration sur les pixels
+                maxi = std::max(max, std::abs((clusters[i].data() + k) - (clustersSave[i].data() + k)));
+            }
+        }
+        end = (maxi < epsilon);
     }
 
 
